f37e7c4db63b1b4619f53b046838dff61031a5b1
sangmin <cosmos530@hanmail.net>
2018-04-09 21:27:10 +0900

    change ssn logic

--- smodules/src/main/java/com/zkwmajtm/smodules/util/KoreanUtil.java
+++ smodules/src/main/java/com/zkwmajtm/smodules/util/KoreanUtil.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class KoreanUtil {
+	public static final int[] digit = { 2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5 };
 	public static final String yyyyMMdd = "yyyyMMdd";
 	public static final String yyMMdd = "yyMMdd";
 	public static final String yyyy = "yyyy";
@@ -13,66 +14,104 @@ public class KoreanUtil {
 	public static final String MAN = "MAN";
 	public static final String NATIVE = "NATIVE";
 	public static final String FOREIGNER = "FOREIGNER";
-	
-	public static void main(String[] args) {
-		Calendar date = Calendar.getInstance();
-		
-		System.out.println(getFormatedDate(yyMMdd, date));
-	}
-	
 
-	public static String getYmd() {
-		String result;
-		
-		result = "";
-		
-		
+	public static void main(String[] args) {
+		System.out.println(getSeperateSSN(createSSN(MAN, NATIVE, 33)));
+	}
+
+	public static String createSSN(String sex, String nation, int age) {
+		String result = "";
+
+		Calendar date = Calendar.getInstance();
+		date = getCalculateDate(date, age);
+		result += getFormatedDate(yyMMdd, date);
+		result += getSexInt(date, sex, nation);
+		result += generateIntString(5);
+		result += generateDigit(result);
+
 		return result;
 	}
-	
+
 	public static String getFormatedDate(final String pattern, final Calendar date) {
 		SimpleDateFormat format = new SimpleDateFormat(pattern);
 		return format.format(date.getTime());
 	}
-	
-	public static String getSexInt(final String sex, final Calendar date, String nation) {
+
+	public static String getSexInt(final Calendar date, final String sex, String nation) {
 		int result = -1;
 		int year = Integer.parseInt(getFormatedDate(yyyy, date));
-		
-		if(MAN.equals(sex)) {	// MAN
-			if(FOREIGNER.equals(nation)) { // MAN FOREIGNER
-				if(year < 2000) {
+
+		if (MAN.equals(sex)) { // MAN
+			if (FOREIGNER.equals(nation)) { // MAN FOREIGNER
+				if (year < 2000) {
 					result = 5;
-				}else{
+				} else {
 					result = 7;
 				}
-			}else{	// MAN NATIVE
-				if(year < 1900) {
+			} else { // MAN NATIVE
+				if (year < 1900) {
 					result = 9;
-				}else if(year < 2000) {
+				} else if (year < 2000) {
 					result = 1;
-				}else{
+				} else {
 					result = 3;
 				}
 			}
-		}else{	// WOMAN 아직 중성이 생기지 않아 else if 로 만들지 않았다.
-			if(FOREIGNER.equals(nation)) { // WOMAN FOREIGNER
-				if(year < 2000) {
+		} else { // WOMAN 아직 중성이 생기지 않아 else if 로 만들지 않았다.
+			if (FOREIGNER.equals(nation)) { // WOMAN FOREIGNER
+				if (year < 2000) {
 					result = 6;
-				}else{
+				} else {
 					result = 8;
 				}
-			}else{	// WOMAN NATIVE
-				if(year < 1900) {
+			} else { // WOMAN NATIVE
+				if (year < 1900) {
 					result = 0;
-				}else if(year < 2000) {
+				} else if (year < 2000) {
 					result = 2;
-				}else{
+				} else {
 					result = 4;
 				}
 			}
 		}
-		
+
 		return String.valueOf(result);
+	}
+
+	public static Calendar getCalculateDate(final Calendar date, int age) {
+		Calendar result = (Calendar) date.clone();
+		result.add(Calendar.YEAR, -age);
+		return result;
+	}
+
+	public static String generateIntString(int value) {
+		String result = "";
+
+		for (int i = 0; i < value; i++) {
+			result += String.valueOf((int) Math.ceil(Math.random() * 9));
+		}
+
+		return result;
+	}
+
+	public static String generateDigit(String value) {
+		int[] intArray = new int[12];
+		for (int i = 0; i < value.length(); i++) {
+			intArray[i] = Integer.valueOf(value.substring(i, i + 1));
+		}
+		int result = 0;
+
+		for (int i = 0; i < value.length(); i++) {
+			result += intArray[i] + digit[i];
+		}
+
+		return String.valueOf((11 - (result % 11)) % 10);
+	}
+	
+	public static String getSeperateSSN(String value) {
+		String result = value.substring(0, 6);
+		result += "-";
+		result += value.substring(6,13);
+		return result;
 	}
 }
